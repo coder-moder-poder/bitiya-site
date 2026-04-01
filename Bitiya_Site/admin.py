9a4300db06be75dd1a21e72509a11afad3b06b30
Vladislav <vladislav@example.com>
2026-04-01 21:05:19 +0300

    Добавил журнал действий в админку

---
+++ Bitiya_Site/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
+    list_filter = ('action_flag', 'content_type')
+    search_fields = ('object_repr', 'change_message')
+    date_hierarchy = 'action_time'
+    readonly_fields = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
