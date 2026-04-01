6e3380c3ce7e1c8d2aea1aa7b57c005c3f66a03d
Vladislav <vladislav@example.com>
2026-04-01 21:15:43 +0300

    Исправил отображение журнала в админке

--- Bitiya_Site/admin.py
+++ Bitiya_Site/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
+from django.contrib.admin.options import ModelAdmin
 
-@admin.register(LogEntry)
-class LogEntryAdmin(admin.ModelAdmin):
+class LogEntryAdmin(ModelAdmin):
     list_display = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
     list_filter = ('action_flag', 'content_type')
     search_fields = ('object_repr', 'change_message')
     date_hierarchy = 'action_time'
     readonly_fields = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
+
+admin.site.register(LogEntry, LogEntryAdmin)
